0c6abf74efc363249f63cb49f025a707ceb2e890
sailing-innocent <sailing-innocent@foxmail.com>
2025-11-28 17:16:13 +0800

    ozz animation runtime

--- thirdparty/xmake.lua
+++ thirdparty/xmake.lua
@@ -6,6 +6,37 @@ includes("tinytiff")
 includes("open_fbx")
 includes("cpp-ipc")
 
+
+target("ozz_animation_base")
+    set_kind("static")
+    add_headerfiles("ozz_animation/include/**.h")
+    add_includedirs("ozz_animation/include", { public = true})
+    add_files("ozz_animation/src/base/**.cc")
+target_end()
+
+target("ozz_animation_runtime")
+    set_kind("shared")
+    add_deps("ozz_animation_base")
+    add_includedirs("ozz_animation/src", { private = true})
+    add_files("ozz_animation/src/animation/runtime/*.cc")
+    add_files("ozz_animation/src/geometry/**.cc")
+    add_files("ozz_animation/src/options/**.cc")
+    add_defines("OZZ_BUILD_ANIMATION_LIB")
+target_end()
+
+
+-- target("ozz_animation_offline")
+--     set_kind("shared")
+--     add_deps("ozz_animation_base")
+--     add_includedirs("ozz_animation/src", { private = true})
+--     -- general
+--     add_files("ozz_animation/src/animation/offline/*.cc")
+--     -- gltf
+--     add_includedirs("ozz_animation/src/animation/offline/gltf", { private = true})
+--     add_files("ozz_animation/src/animation/offline/gltf/*.cc")
+-- target_end()
+
+
 target("qt_node_editor")
     set_kind("shared")
     add_rules("qt.rbc_shared")
