78906545902637a3f4799a1197aba614d8b5a839
sailing-innocent <sailing-innocent@foxmail.com>
2025-11-28 19:32:58 +0800

    add some [[nodiscard]]

--- rbc/runtime/include/rbc_graphics/mesh_builder.h
+++ rbc/runtime/include/rbc_graphics/mesh_builder.h
@@ -6,26 +6,25 @@
 #include <luisa/vstl/common.h>
 #include <luisa/core/stl/string.h>
 #include <rbc_config.h>
-namespace rbc
-{
+namespace rbc {
 using namespace luisa;
-template <typename Derive>
+template<typename Derive>
 struct MeshBuilderBase {
-    uint vertex_count() const;
-    bool contained_normal() const;
-    bool contained_tangent() const;
-    uint uv_count() const;
-    uint submesh_count() const;
-    uint indices_count() const;
-    luisa::span<uint const> get_submesh_indices(size_t submesh_index) const;
-    luisa::string check() const; // return: error message
+    [[nodiscard]] uint vertex_count() const;
+    [[nodiscard]] bool contained_normal() const;
+    [[nodiscard]] bool contained_tangent() const;
+    [[nodiscard]] uint uv_count() const;
+    [[nodiscard]] uint submesh_count() const;
+    [[nodiscard]] uint indices_count() const;
+    [[nodiscard]] luisa::span<uint const> get_submesh_indices(size_t submesh_index) const;
+    [[nodiscard]] luisa::string check() const;// return: error message
     // property write
 
-    void write_to(luisa::filesystem::path const& dst_path, luisa::vector<uint>& submesh_offsets) const;
-    void write_to(luisa::vector<std::byte>& buffer, luisa::vector<uint>& submesh_offsets) const;
+    void write_to(luisa::filesystem::path const &dst_path, luisa::vector<uint> &submesh_offsets) const;
+    void write_to(luisa::vector<std::byte> &buffer, luisa::vector<uint> &submesh_offsets) const;
 
 private:
-    void _gen_submesh_offsets(luisa::vector<uint>& submesh_offsets) const;
+    void _gen_submesh_offsets(luisa::vector<uint> &submesh_offsets) const;
 };
 struct RBC_RUNTIME_API MeshBuilder : MeshBuilderBase<MeshBuilder> {
     using BaseType = MeshBuilderBase<MeshBuilder>;
@@ -37,31 +36,30 @@ struct RBC_RUNTIME_API MeshBuilder : MeshBuilderBase<MeshBuilder> {
 
     MeshBuilder();
     ~MeshBuilder();
-    MeshBuilder(MeshBuilder&& rhs);
-    MeshBuilder(MeshBuilder const&) = delete;
-    MeshBuilder& operator=(MeshBuilder const&) = delete;
-    MeshBuilder& operator=(MeshBuilder&& rhs)
-    {
+    MeshBuilder(MeshBuilder &&rhs) noexcept;
+    MeshBuilder(MeshBuilder const &) = delete;
+    MeshBuilder &operator=(MeshBuilder const &) = delete;
+    MeshBuilder &operator=(MeshBuilder &&rhs) noexcept {
         std::destroy_at(this);
         vstd::construct_at(this, std::move(rhs));
         return *this;
     }
     // property read
-    uint vertex_count() const;
-    bool contained_normal() const;
-    bool contained_tangent() const;
-    uint uv_count() const;
-    uint submesh_count() const;
-    uint indices_count() const;
-    luisa::span<uint const> get_submesh_indices(size_t submesh_index) const;
-    luisa::string check() const; // return: error message
+    [[nodiscard]] uint vertex_count() const;
+    [[nodiscard]] bool contained_normal() const;
+    [[nodiscard]] bool contained_tangent() const;
+    [[nodiscard]] uint uv_count() const;
+    [[nodiscard]] uint submesh_count() const;
+    [[nodiscard]] uint indices_count() const;
+    [[nodiscard]] luisa::span<uint const> get_submesh_indices(size_t submesh_index) const;
+    [[nodiscard]] luisa::string check() const;// return: error message
     // property write
 
-    void write_to(luisa::filesystem::path const& dst_path, luisa::vector<uint>& submesh_offsets) const;
-    void write_to(luisa::vector<std::byte>& buffer, luisa::vector<uint>& submesh_offsets) const;
+    void write_to(luisa::filesystem::path const &dst_path, luisa::vector<uint> &submesh_offsets) const;
+    void write_to(luisa::vector<std::byte> &buffer, luisa::vector<uint> &submesh_offsets) const;
 
 private:
-    void _gen_submesh_offsets(luisa::vector<uint>& submesh_offsets) const;
+    void _gen_submesh_offsets(luisa::vector<uint> &submesh_offsets) const;
 };
 struct RBC_RUNTIME_API MeshBuilderSpan : MeshBuilderBase<MeshBuilderSpan> {
     using BaseType = MeshBuilderBase<MeshBuilderSpan>;
@@ -73,30 +71,29 @@ struct RBC_RUNTIME_API MeshBuilderSpan : MeshBuilderBase<MeshBuilderSpan> {
 
     MeshBuilderSpan();
     ~MeshBuilderSpan();
-    MeshBuilderSpan(MeshBuilderSpan&& rhs);
-    MeshBuilderSpan(MeshBuilderSpan const&) = delete;
-    MeshBuilderSpan& operator=(MeshBuilderSpan const&) = delete;
-    MeshBuilderSpan& operator=(MeshBuilderSpan&& rhs)
-    {
+    MeshBuilderSpan(MeshBuilderSpan &&rhs) noexcept;
+    MeshBuilderSpan(MeshBuilderSpan const &) = delete;
+    MeshBuilderSpan &operator=(MeshBuilderSpan const &) = delete;
+    MeshBuilderSpan &operator=(MeshBuilderSpan &&rhs) noexcept {
         std::destroy_at(this);
         vstd::construct_at(this, std::move(rhs));
         return *this;
     }
     // property read
-    uint vertex_count() const;
-    bool contained_normal() const;
-    bool contained_tangent() const;
-    uint uv_count() const;
-    uint submesh_count() const;
-    uint indices_count() const;
-    luisa::span<uint const> get_submesh_indices(size_t submesh_index) const;
-    luisa::string check() const; // return: error message
+    [[nodiscard]] uint vertex_count() const;
+    [[nodiscard]] bool contained_normal() const;
+    [[nodiscard]] bool contained_tangent() const;
+    [[nodiscard]] uint uv_count() const;
+    [[nodiscard]] uint submesh_count() const;
+    [[nodiscard]] uint indices_count() const;
+    [[nodiscard]] luisa::span<uint const> get_submesh_indices(size_t submesh_index) const;
+    [[nodiscard]] luisa::string check() const;// return: error message
     // property write
 
-    void write_to(luisa::filesystem::path const& dst_path, luisa::vector<uint>& submesh_offsets) const;
-    void write_to(luisa::vector<std::byte>& buffer, luisa::vector<uint>& submesh_offsets) const;
+    void write_to(luisa::filesystem::path const &dst_path, luisa::vector<uint> &submesh_offsets) const;
+    void write_to(luisa::vector<std::byte> &buffer, luisa::vector<uint> &submesh_offsets) const;
 
 private:
-    void _gen_submesh_offsets(luisa::vector<uint>& submesh_offsets) const;
+    void _gen_submesh_offsets(luisa::vector<uint> &submesh_offsets) const;
 };
-} // namespace rbc
+}// namespace rbc
--- rbc/runtime/src/graphics/mesh_builder.cpp
+++ rbc/runtime/src/graphics/mesh_builder.cpp
@@ -3,97 +3,72 @@
 #include <luisa/core/stl/format.h>
 #include <luisa/core/logging.h>
 
-namespace rbc
-{
-MeshBuilder::MeshBuilder()
-{
+namespace rbc {
+MeshBuilder::MeshBuilder() = default;
+MeshBuilder::~MeshBuilder() {}
+MeshBuilder::MeshBuilder(MeshBuilder &&rhs) noexcept = default;
+MeshBuilderSpan::MeshBuilderSpan() = default;
+MeshBuilderSpan::~MeshBuilderSpan() {}
+MeshBuilderSpan::MeshBuilderSpan(MeshBuilderSpan &&rhs) noexcept = default;
+template<typename Derive>
+uint MeshBuilderBase<Derive>::vertex_count() const {
+    return static_cast<Derive const *>(this)->position.size();
 }
-MeshBuilder::~MeshBuilder()
-{
+template<typename Derive>
+bool MeshBuilderBase<Derive>::contained_normal() const {
+    return !static_cast<Derive const *>(this)->normal.empty();
 }
-MeshBuilder::MeshBuilder(MeshBuilder&& rhs) = default;
-MeshBuilderSpan::MeshBuilderSpan()
-{
+template<typename Derive>
+bool MeshBuilderBase<Derive>::contained_tangent() const {
+    return !static_cast<Derive const *>(this)->tangent.empty();
 }
-MeshBuilderSpan::~MeshBuilderSpan()
-{
+template<typename Derive>
+uint MeshBuilderBase<Derive>::uv_count() const {
+    return static_cast<Derive const *>(this)->uvs.size();
 }
-MeshBuilderSpan::MeshBuilderSpan(MeshBuilderSpan&& rhs) = default;
-template <typename Derive>
-uint MeshBuilderBase<Derive>::vertex_count() const
-{
-    return static_cast<Derive const*>(this)->position.size();
+template<typename Derive>
+uint MeshBuilderBase<Derive>::submesh_count() const {
+    return static_cast<Derive const *>(this)->triangle_indices.size();
 }
-template <typename Derive>
-bool MeshBuilderBase<Derive>::contained_normal() const
-{
-    return !static_cast<Derive const*>(this)->normal.empty();
-}
-template <typename Derive>
-bool MeshBuilderBase<Derive>::contained_tangent() const
-{
-    return !static_cast<Derive const*>(this)->tangent.empty();
-}
-template <typename Derive>
-uint MeshBuilderBase<Derive>::uv_count() const
-{
-    return static_cast<Derive const*>(this)->uvs.size();
-}
-template <typename Derive>
-uint MeshBuilderBase<Derive>::submesh_count() const
-{
-    return static_cast<Derive const*>(this)->triangle_indices.size();
-}
-template <typename Derive>
-uint MeshBuilderBase<Derive>::indices_count() const
-{
+template<typename Derive>
+uint MeshBuilderBase<Derive>::indices_count() const {
     uint result = 0;
-    for (auto& i : static_cast<Derive const*>(this)->triangle_indices)
-    {
+    for (auto &i : static_cast<Derive const *>(this)->triangle_indices) {
         result += i.size();
     }
     return result;
 }
-template <typename Derive>
-luisa::string MeshBuilderBase<Derive>::check() const
-{
+template<typename Derive>
+luisa::string MeshBuilderBase<Derive>::check() const {
     luisa::string result;
-    auto&& position = static_cast<Derive const*>(this)->position;
-    auto&& normal = static_cast<Derive const*>(this)->normal;
-    auto&& tangent = static_cast<Derive const*>(this)->tangent;
-    auto&& uvs = static_cast<Derive const*>(this)->uvs;
-    auto&& triangle_indices = static_cast<Derive const*>(this)->triangle_indices;
+    auto &&position = static_cast<Derive const *>(this)->position;
+    auto &&normal = static_cast<Derive const *>(this)->normal;
+    auto &&tangent = static_cast<Derive const *>(this)->tangent;
+    auto &&uvs = static_cast<Derive const *>(this)->uvs;
+    auto &&triangle_indices = static_cast<Derive const *>(this)->triangle_indices;
 
-    if (!normal.empty() && normal.size() != position.size()) [[unlikely]]
-    {
+    if (!normal.empty() && normal.size() != position.size()) [[unlikely]] {
         result += luisa::format("Normal size {} not match position size. {}\n", normal.size(), position.size());
     };
-    if (!tangent.empty() && tangent.size() != position.size()) [[unlikely]]
-    {
+    if (!tangent.empty() && tangent.size() != position.size()) [[unlikely]] {
         result += luisa::format("Tangent size {} not match position size. {}\n", tangent.size(), position.size());
     };
     size_t idx = 0;
-    for (auto& uv : uvs)
-    {
-        if (!uv.empty() && uv.size() != position.size()) [[unlikely]]
-        {
+    for (auto &uv : uvs) {
+        if (!uv.empty() && uv.size() != position.size()) [[unlikely]] {
             result += luisa::format("UV{} size {} not match position size. {}\n", idx, uv.size(), position.size());
         };
         ++idx;
     }
     idx = 0;
-    for (auto& i : triangle_indices)
-    {
-        if ((i.size() % 3) != 0) [[unlikely]]
-        {
+    for (auto &i : triangle_indices) {
+        if ((i.size() % 3) != 0) [[unlikely]] {
             result += luisa::format("Submesh {} size {} is illegal.\n", idx, i.size());
             ++idx;
             continue;
         }
-        for (auto& j : i)
-        {
-            if (j >= position.size()) [[unlikely]]
-            {
+        for (auto &j : i) {
+            if (j >= position.size()) [[unlikely]] {
                 result += luisa::format("At least one of indices in submesh {} out of vertex range {}.\n", idx, position.size());
                 break;
             }
@@ -102,185 +77,151 @@ luisa::string MeshBuilderBase<Derive>::check() const
     }
     return result;
 }
-template <typename Derive>
-luisa::span<uint const> MeshBuilderBase<Derive>::get_submesh_indices(size_t submesh_index) const
-{
-    auto&& triangle_indices = static_cast<Derive const*>(this)->triangle_indices;
-    return { triangle_indices[submesh_index] };
+template<typename Derive>
+luisa::span<uint const> MeshBuilderBase<Derive>::get_submesh_indices(size_t submesh_index) const {
+    auto &&triangle_indices = static_cast<Derive const *>(this)->triangle_indices;
+    return {triangle_indices[submesh_index]};
 }
 
-template <typename Derive>
-void MeshBuilderBase<Derive>::write_to(luisa::filesystem::path const& dst_path, luisa::vector<uint>& submesh_offsets) const
-{
-    auto&& position = static_cast<Derive const*>(this)->position;
-    auto&& normal = static_cast<Derive const*>(this)->normal;
-    auto&& tangent = static_cast<Derive const*>(this)->tangent;
-    auto&& uvs = static_cast<Derive const*>(this)->uvs;
-    auto&& triangle_indices = static_cast<Derive const*>(this)->triangle_indices;
+template<typename Derive>
+void MeshBuilderBase<Derive>::write_to(luisa::filesystem::path const &dst_path, luisa::vector<uint> &submesh_offsets) const {
+    auto &&position = static_cast<Derive const *>(this)->position;
+    auto &&normal = static_cast<Derive const *>(this)->normal;
+    auto &&tangent = static_cast<Derive const *>(this)->tangent;
+    auto &&uvs = static_cast<Derive const *>(this)->uvs;
+    auto &&triangle_indices = static_cast<Derive const *>(this)->triangle_indices;
 #ifndef NDEBUG
     auto err_msg = check();
-    if (!err_msg.empty()) [[unlikely]]
-    {
+    if (!err_msg.empty()) [[unlikely]] {
         LUISA_ERROR("{}", err_msg);
     }
 #endif
-    BinaryFileWriter writer{ luisa::to_string(dst_path) };
-    writer.write({ reinterpret_cast<const std::byte*>(position.data()), position.size_bytes() });
-    writer.write({ reinterpret_cast<const std::byte*>(normal.data()), normal.size_bytes() });
-    writer.write({ reinterpret_cast<const std::byte*>(tangent.data()), tangent.size_bytes() });
-    for (auto& uv : uvs)
-    {
-        writer.write({ reinterpret_cast<const std::byte*>(uv.data()), uv.size_bytes() });
+    BinaryFileWriter writer{luisa::to_string(dst_path)};
+    writer.write({reinterpret_cast<const std::byte *>(position.data()), position.size_bytes()});
+    writer.write({reinterpret_cast<const std::byte *>(normal.data()), normal.size_bytes()});
+    writer.write({reinterpret_cast<const std::byte *>(tangent.data()), tangent.size_bytes()});
+    for (auto &uv : uvs) {
+        writer.write({reinterpret_cast<const std::byte *>(uv.data()), uv.size_bytes()});
     }
-    for (auto& i : triangle_indices)
-    {
-        writer.write({ reinterpret_cast<const std::byte*>(i.data()), i.size_bytes() });
+    for (auto &i : triangle_indices) {
+        writer.write({reinterpret_cast<const std::byte *>(i.data()), i.size_bytes()});
     }
     _gen_submesh_offsets(submesh_offsets);
 }
-template <typename Derive>
-void MeshBuilderBase<Derive>::write_to(luisa::vector<std::byte>& buffer, luisa::vector<uint>& submesh_offsets) const
-{
-    auto&& position = static_cast<Derive const*>(this)->position;
-    auto&& normal = static_cast<Derive const*>(this)->normal;
-    auto&& tangent = static_cast<Derive const*>(this)->tangent;
-    auto&& uvs = static_cast<Derive const*>(this)->uvs;
-    auto&& triangle_indices = static_cast<Derive const*>(this)->triangle_indices;
+template<typename Derive>
+void MeshBuilderBase<Derive>::write_to(luisa::vector<std::byte> &buffer, luisa::vector<uint> &submesh_offsets) const {
+    auto &&position = static_cast<Derive const *>(this)->position;
+    auto &&normal = static_cast<Derive const *>(this)->normal;
+    auto &&tangent = static_cast<Derive const *>(this)->tangent;
+    auto &&uvs = static_cast<Derive const *>(this)->uvs;
+    auto &&triangle_indices = static_cast<Derive const *>(this)->triangle_indices;
 #ifndef NDEBUG
     auto err_msg = check();
-    if (!err_msg.empty()) [[unlikely]]
-    {
+    if (!err_msg.empty()) [[unlikely]] {
         LUISA_ERROR("{}", err_msg);
     }
 #endif
     buffer.clear();
     size_t capacity = position.size_bytes() + normal.size_bytes() + tangent.size_bytes();
-    for (auto& uv : uvs)
-    {
+    for (auto &uv : uvs) {
         capacity += uv.size_bytes();
     }
-    for (auto& i : triangle_indices)
-    {
+    for (auto &i : triangle_indices) {
         capacity += i.size_bytes();
     }
     buffer.reserve(capacity);
-    vstd::push_back_all(buffer, { reinterpret_cast<const std::byte*>(position.data()), position.size_bytes() });
-    vstd::push_back_all(buffer, { reinterpret_cast<const std::byte*>(normal.data()), normal.size_bytes() });
-    vstd::push_back_all(buffer, { reinterpret_cast<const std::byte*>(tangent.data()), tangent.size_bytes() });
-    for (auto& uv : uvs)
-    {
-        vstd::push_back_all(buffer, { reinterpret_cast<const std::byte*>(uv.data()), uv.size_bytes() });
+    vstd::push_back_all(buffer, {reinterpret_cast<const std::byte *>(position.data()), position.size_bytes()});
+    vstd::push_back_all(buffer, {reinterpret_cast<const std::byte *>(normal.data()), normal.size_bytes()});
+    vstd::push_back_all(buffer, {reinterpret_cast<const std::byte *>(tangent.data()), tangent.size_bytes()});
+    for (auto &uv : uvs) {
+        vstd::push_back_all(buffer, {reinterpret_cast<const std::byte *>(uv.data()), uv.size_bytes()});
     }
-    for (auto& i : triangle_indices)
-    {
-        vstd::push_back_all(buffer, { reinterpret_cast<const std::byte*>(i.data()), i.size_bytes() });
+    for (auto &i : triangle_indices) {
+        vstd::push_back_all(buffer, {reinterpret_cast<const std::byte *>(i.data()), i.size_bytes()});
     }
     _gen_submesh_offsets(submesh_offsets);
 }
-template <typename Derive>
-void MeshBuilderBase<Derive>::_gen_submesh_offsets(luisa::vector<uint>& submesh_offsets) const
-{
-    auto&& position = static_cast<Derive const*>(this)->position;
-    auto&& normal = static_cast<Derive const*>(this)->normal;
-    auto&& tangent = static_cast<Derive const*>(this)->tangent;
-    auto&& uvs = static_cast<Derive const*>(this)->uvs;
-    auto&& triangle_indices = static_cast<Derive const*>(this)->triangle_indices;
+template<typename Derive>
+void MeshBuilderBase<Derive>::_gen_submesh_offsets(luisa::vector<uint> &submesh_offsets) const {
+    auto &&position = static_cast<Derive const *>(this)->position;
+    auto &&normal = static_cast<Derive const *>(this)->normal;
+    auto &&tangent = static_cast<Derive const *>(this)->tangent;
+    auto &&uvs = static_cast<Derive const *>(this)->uvs;
+    auto &&triangle_indices = static_cast<Derive const *>(this)->triangle_indices;
     submesh_offsets.clear();
-    if (triangle_indices.size() <= 1)
-    {
+    if (triangle_indices.size() <= 1) {
         return;
     }
     size_t offset = 0;
     submesh_offsets.reserve(triangle_indices.size());
-    for (auto& i : triangle_indices)
-    {
+    for (auto &i : triangle_indices) {
         submesh_offsets.emplace_back(offset);
         offset += i.size() / 3;
     }
 }
-uint MeshBuilder::vertex_count() const
-{
+uint MeshBuilder::vertex_count() const {
     return BaseType::vertex_count();
 }
-bool MeshBuilder::contained_normal() const
-{
+bool MeshBuilder::contained_normal() const {
     return BaseType::contained_normal();
 }
-bool MeshBuilder::contained_tangent() const
-{
+bool MeshBuilder::contained_tangent() const {
     return BaseType::contained_tangent();
 }
-uint MeshBuilder::uv_count() const
-{
+uint MeshBuilder::uv_count() const {
     return BaseType::uv_count();
 }
-uint MeshBuilder::submesh_count() const
-{
+uint MeshBuilder::submesh_count() const {
     return BaseType::submesh_count();
 }
-uint MeshBuilder::indices_count() const
-{
+uint MeshBuilder::indices_count() const {
     return BaseType::indices_count();
 }
-luisa::span<uint const> MeshBuilder::get_submesh_indices(size_t submesh_index) const
-{
+luisa::span<uint const> MeshBuilder::get_submesh_indices(size_t submesh_index) const {
     return BaseType::get_submesh_indices(submesh_index);
 }
-luisa::string MeshBuilder::check() const
-{
+luisa::string MeshBuilder::check() const {
     return BaseType::check();
 }
 // property write
 
-void MeshBuilder::write_to(luisa::filesystem::path const& dst_path, luisa::vector<uint>& submesh_offsets) const
-{
+void MeshBuilder::write_to(luisa::filesystem::path const &dst_path, luisa::vector<uint> &submesh_offsets) const {
     BaseType::write_to(dst_path, submesh_offsets);
 }
-void MeshBuilder::write_to(luisa::vector<std::byte>& buffer, luisa::vector<uint>& submesh_offsets) const
-{
+void MeshBuilder::write_to(luisa::vector<std::byte> &buffer, luisa::vector<uint> &submesh_offsets) const {
     BaseType::write_to(buffer, submesh_offsets);
 }
 
-uint MeshBuilderSpan::vertex_count() const
-{
+uint MeshBuilderSpan::vertex_count() const {
     return BaseType::vertex_count();
 }
-bool MeshBuilderSpan::contained_normal() const
-{
+bool MeshBuilderSpan::contained_normal() const {
     return BaseType::contained_normal();
 }
-bool MeshBuilderSpan::contained_tangent() const
-{
+bool MeshBuilderSpan::contained_tangent() const {
     return BaseType::contained_tangent();
 }
-uint MeshBuilderSpan::uv_count() const
-{
+uint MeshBuilderSpan::uv_count() const {
     return BaseType::uv_count();
 }
-uint MeshBuilderSpan::submesh_count() const
-{
+uint MeshBuilderSpan::submesh_count() const {
     return BaseType::submesh_count();
 }
-uint MeshBuilderSpan::indices_count() const
-{
+uint MeshBuilderSpan::indices_count() const {
     return BaseType::indices_count();
 }
-luisa::span<uint const> MeshBuilderSpan::get_submesh_indices(size_t submesh_index) const
-{
+luisa::span<uint const> MeshBuilderSpan::get_submesh_indices(size_t submesh_index) const {
     return BaseType::get_submesh_indices(submesh_index);
 }
-luisa::string MeshBuilderSpan::check() const
-{
+luisa::string MeshBuilderSpan::check() const {
     return BaseType::check();
 }
 // property write
 
-void MeshBuilderSpan::write_to(luisa::filesystem::path const& dst_path, luisa::vector<uint>& submesh_offsets) const
-{
+void MeshBuilderSpan::write_to(luisa::filesystem::path const &dst_path, luisa::vector<uint> &submesh_offsets) const {
     BaseType::write_to(dst_path, submesh_offsets);
 }
-void MeshBuilderSpan::write_to(luisa::vector<std::byte>& buffer, luisa::vector<uint>& submesh_offsets) const
-{
+void MeshBuilderSpan::write_to(luisa::vector<std::byte> &buffer, luisa::vector<uint> &submesh_offsets) const {
     BaseType::write_to(buffer, submesh_offsets);
 }
-} // namespace rbc
+}// namespace rbc
