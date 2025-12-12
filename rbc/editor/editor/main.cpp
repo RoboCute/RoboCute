44e939d67e0337483848b6d51e90c914ee6c12ed
sailing-innocent <sailing-innocent@foxmail.com>
2025-12-12 13:48:18 +0800

    add argc and argv

--- rbc/editor/editor/main.cpp
+++ rbc/editor/editor/main.cpp
@@ -1,5 +1,7 @@
 #include <QApplication>
 #include <QFile>
+#include <string>
+#include <cstdlib>
 
 #include <rbc_graphics/scene_manager.h>
 #include <rbc_graphics/shader_manager.h>
@@ -21,7 +23,6 @@
 
 #include "RBCEditorRuntime/MainWindow.h"
 #include "RBCEditorRuntime/engine/EditorEngine.h"
-#include "RBCEditorRuntime/runtime/RenderScene.h"
 
 #ifdef _WIN32
 #include <windows.h>
@@ -34,44 +35,116 @@ using namespace luisa;
 using namespace luisa::compute;
 #include <material/mats.inl>
 
+struct AppConfig {
+    bool enable_console = true;  // 默认打开 console
+    bool enable_debug = true;    // 默认开启 debug 日志
+    int server_port = 5555;      // 默认端口 5555
+};
+
+void print_usage(const char *program_name) {
+    printf("Usage: %s [OPTIONS]\n", program_name);
+    printf("Options:\n");
+    printf("  -c, --console          Enable console window (default: enabled)\n");
+    printf("  --no-console           Disable console window\n");
+    printf("  -d, --debug            Enable debug logging (default: enabled)\n");
+    printf("  --no-debug             Disable debug logging\n");
+    printf("  -p, --port PORT        Set server port (default: 5555)\n");
+    printf("  -h, --help             Show this help message\n");
+}
+
+AppConfig parse_arguments(int argc, char **argv) {
+    AppConfig config;
+    
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        
+        if (arg == "-h" || arg == "--help") {
+            print_usage(argv[0]);
+            exit(0);
+        } else if (arg == "-c" || arg == "--console") {
+            config.enable_console = true;
+        } else if (arg == "--no-console") {
+            config.enable_console = false;
+        } else if (arg == "-d" || arg == "--debug") {
+            config.enable_debug = true;
+        } else if (arg == "--no-debug") {
+            config.enable_debug = false;
+        } else if (arg == "-p" || arg == "--port") {
+            if (i + 1 < argc) {
+                try {
+                    config.server_port = std::stoi(argv[++i]);
+                    if (config.server_port < 1 || config.server_port > 65535) {
+                        fprintf(stderr, "Error: Port must be between 1 and 65535\n");
+                        exit(1);
+                    }
+                } catch (...) {
+                    fprintf(stderr, "Error: Invalid port number: %s\n", argv[i]);
+                    exit(1);
+                }
+            } else {
+                fprintf(stderr, "Error: --port requires a port number\n");
+                exit(1);
+            }
+        } else {
+            fprintf(stderr, "Error: Unknown option: %s\n", arg.c_str());
+            fprintf(stderr, "Use --help for usage information\n");
+            exit(1);
+        }
+    }
+    
+    return config;
+}
+
 int main(int argc, char **argv) {
+    // Parse command line arguments
+    AppConfig config = parse_arguments(argc, argv);
 
 #ifdef _WIN32
-    // Allocate console window for debug output on Windows
-    AllocConsole();
+    // Allocate console window for debug output on Windows (if enabled)
+    if (config.enable_console) {
+        AllocConsole();
 
-    // Redirect stdout and stderr to console
-    FILE *pCout;
-    FILE *pCerr;
-    FILE *pCin;
-    freopen_s(&pCout, "CONOUT$", "w", stdout);
-    freopen_s(&pCerr, "CONOUT$", "w", stderr);
-    freopen_s(&pCin, "CONIN$", "r", stdin);
+        // Redirect stdout and stderr to console
+        FILE *pCout;
+        FILE *pCerr;
+        FILE *pCin;
+        freopen_s(&pCout, "CONOUT$", "w", stdout);
+        freopen_s(&pCerr, "CONOUT$", "w", stderr);
+        freopen_s(&pCin, "CONIN$", "r", stdin);
 
-    // Set console title
-    SetConsoleTitleA("RoboCute Editor - Debug Console");
+        // Set console title
+        SetConsoleTitleA("RoboCute Editor - Debug Console");
+    }
 #endif
 
     int ret = 0;
-    log_level_info();
-
-    if (true) {
-        QApplication app(argc, argv);
-        QFile f(":/main.qss");
-        QString styleSheet = "";
-        if (f.open(QIODevice::ReadOnly)) {
-            styleSheet = f.readAll();
-        }
-        app.setStyleSheet(styleSheet);
-        rbc::EditorEngine::instance().init(argc, argv);
-        {
-            MainWindow window;
-            window.setupUi();
-            window.startSceneSync("http://127.0.0.1:5555");
-            window.show();
-            ret = QApplication::exec();
-        }
-        rbc::EditorEngine::instance().shutdown();
+    
+    // Set log level based on debug flag
+    if (config.enable_debug) {
+        log_level_info();
+    } else {
+        log_level_error();
     }
+
+    QApplication app(argc, argv);
+    QFile f(":/main.qss");
+    QString styleSheet = "";
+    if (f.open(QIODevice::ReadOnly)) {
+        styleSheet = f.readAll();
+    }
+    app.setStyleSheet(styleSheet);
+    rbc::EditorEngine::instance().init(argc, argv);
+    {
+        MainWindow window;
+        window.setupUi();
+        
+        // Build server URL with configured port
+        std::string server_url = "http://127.0.0.1:" + std::to_string(config.server_port);
+        window.startSceneSync(QString::fromStdString(server_url));
+        window.show();
+        ret = QApplication::exec();
+    }
+    rbc::EditorEngine::instance().shutdown();
+    
     return ret;
 }
