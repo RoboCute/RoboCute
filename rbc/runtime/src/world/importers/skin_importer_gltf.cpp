309807a02df4bc010dda7ed96532fa73f4087b0e
Yuefeng Geng <maxwellgeng@outlook.com>
2025-12-26 20:08:21 +0800

    Fix build, fix texture alignment

--- rbc/runtime/src/world/importers/skin_importer_gltf.cpp
+++ rbc/runtime/src/world/importers/skin_importer_gltf.cpp
@@ -3,9 +3,11 @@
 namespace rbc {
 
 bool GltfSkinImporter::import(SkinResource *resource, luisa::filesystem::path const &path) {
+    return false;
 }
 bool GltfSkinImporter::import_from_data(SkinResource *resource, GltfImportData &import_data) {
     // 由于ozz::math::Float4x4 和 gltf matrix 都是列主序，所以可以直接复制
+    return false;
 }
 
 }// namespace rbc
