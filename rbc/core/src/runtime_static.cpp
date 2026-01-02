eed786861a4b94c180d01d6f65259bd3f34ffa98
Maxwell Geng <33229754+MaxwellGengYF@users.noreply.github.com>
2026-01-02 22:51:38 +0800

    runtime static assert

--- rbc/core/src/runtime_static.cpp
+++ rbc/core/src/runtime_static.cpp
@@ -2,13 +2,18 @@
 
 namespace rbc {
 static RuntimeStaticBase *_runtime_static_header{};
+static bool _runtime_already_loaded{false};
 RuntimeStaticBase::RuntimeStaticBase() {
+    if (_runtime_already_loaded) [[unlikely]] {
+        LUISA_ERROR("RuntimeStatic<> class can not be defined as non-static variable or defiend in plugins.");
+    }
     p_next = _runtime_static_header;
     _runtime_static_header = this;
 }
 RuntimeStaticBase::~RuntimeStaticBase() {
 }
 void RuntimeStaticBase::init_all() {
+    _runtime_already_loaded = true;
     for (auto p = _runtime_static_header; p; p = p->p_next) {
         p->init();
     }
